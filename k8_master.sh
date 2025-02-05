af1c9e7c401e81d0582a315e02648dcbd8ee0a98
Rishikesh Gulhane <99083310+RishikeshMech@users.noreply.github.com>
2025-02-05 10:53:19 +0530

    Update k8_master.sh

--- k8_master.sh
+++ k8_master.sh
@@ -145,3 +145,18 @@ sudo systemctl status kubelet --no-pager | head -n 20
 
 
 sudo kubeadm reset pre-flight checks
+
+#Only Master Node 
+sudo kubeadm config images pull
+
+sudo kubeadm init --pod-network-cidr="192.168.0.0/16"
+
+mkdir -p "$HOME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+
+# Network Plugin = calico
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml
+
+kubeadm token create --print-join-command
